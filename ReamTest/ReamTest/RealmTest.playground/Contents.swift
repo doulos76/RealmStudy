72595f67e513b7976a9f381b788ee0bbdf66a9fe
minhoi_goo <minhoi_goo@mac-fingersui-MacBook-Pro.local>
2019-06-17 21:55:33 +0900

    add Realm Project

---
+++ ReamTest/ReamTest/RealmTest.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+import RealmSwift
+import CoreLocation
+
+
+// Setup
+let realm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: "TemporaryRealm"))
+
+// Playground
+
+print("Ready to play")
+
+
---
+++ ReamTest/ReamTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReamTest
+//
+//  Created by minhoi_goo on 17/06/2019.
+//  Copyright © 2019 minhoi_goo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
